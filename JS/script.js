051cfd2b2430ca8f7229697801aa71431c47fd9e
Arinchiz <146980400+Arinchiz@users.noreply.github.com>
2023-10-15 22:22:15 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,74 @@
+//2
+let a1= 5%3;
+console.log(a1, typeof a1);
+let a2= 3%5;
+console.log(a2, typeof a2);
+let a3= 5+'3';
+console.log(a3, typeof a3);
+let a4= '5'-3;
+console.log(a4, typeof a4);
+let a5= 75+'кг';
+console.log(a5, typeof a5);
+let a6= 758*653;
+console.log(a6, typeof a6);
+let a7= 100/25;
+console.log(a7, typeof a7);
+let a8= 0*0;
+console.log(a8, typeof a8);
+let a9= 0/2;
+console.log(a9, typeof a9);
+let a10= 89/0;
+console.log(a10, typeof a10);
+let a11= 98+2;
+console.log(a11, typeof a11);
+let a12= 5-98;
+console.log(a12, typeof a12);
+let a13= (8+56*4)/5;
+console.log(a13, typeof a13);
+let a14= (9-12)*7/(5+2);
+console.log(a14, typeof a14);
+let a15= +"123";
+console.log(a15, typeof a15);
+let a16= 1||0;
+console.log(a16, typeof a16);
+let a17= false|| true;
+console.log(a17, typeof a17);
+let a18= true>0;
+console.log(a18, typeof a18);
+//3
+let width = 23;
+let height = 10;
+let S= width*height;
+console.log(S)
+//4
+let h=10;
+let d=a7;
+let r=d/2;
+let VCilindra=r*r*h*Math.PI;
+console.log(VCilindra);
+//5
+let r1=5;
+let SKruga=Math.PI*r1*r1;
+console.log(SKruga);
+//6
+let a=5;
+let b=7;
+let h2=10;
+let STrap=(a+b)*h/2;
+console.log(STrap);
+//7
+let S1=2000000;
+let p=0.1;
+let year=5;
+let Pereplata=S1*p*5;
+console.log(Pereplata);
+//8
+let c = 8;
+let e = 3;
+let x1 = (16 - c) / 2 + e;
+console.log(x1);
+let x2 = (c - 15 * e) / (e - 6);
+console.log(x2);
+let x3 = 23780 / (1 + 2 + c + e);
+console.log(x3);
+
